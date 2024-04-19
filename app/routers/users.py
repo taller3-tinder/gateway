d02518a43759e7af557013032f223661199a0919
mateocapon <mcapon@fi.uba.ar>
2024-04-19 13:39:52 -0300

    add docker env

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,8 +1,13 @@
+import os
 from typing import Annotated
+from dotenv import load_dotenv
 from fastapi import APIRouter, Depends
+from fastapi.responses import JSONResponse
 from ..middleware.firebase import get_current_user
 from ..schemas.users import RegisterUser
 from fastapi.encoders import jsonable_encoder
+import requests
+
 
 router = APIRouter(
     prefix="/users",
@@ -13,10 +18,13 @@ router = APIRouter(
     # a este metodo.
 )
 
+load_dotenv()
+USERS_URL = os.getenv("USERS_URL")
+
 
 def join_attributes(user_schema, current_user):
     user = jsonable_encoder(user_schema)
-    user["uid"] = current_user["uid"]
+    user["id"] = current_user["uid"]
     user["email"] = current_user["email"]
     return user
 
@@ -24,24 +32,27 @@ def join_attributes(user_schema, current_user):
 @router.get('/{user_id}')
 def find_user(user_id: str,
               current_user: Annotated[dict, Depends(get_current_user)]):
-    print(f'El current user es {current_user["uid"]}')
-    print(f'El user a obtener es {user_id}')
+    resp = requests.get(f"{USERS_URL}/users/{user_id}/{current_user['uid']}")
+    return JSONResponse(content=resp.json(), status_code=resp.status_code)
 
 
-@router.post('/register')
+@router.post('/')
 def register_user(user_register: RegisterUser,
                   current_user: Annotated[str, Depends(get_current_user)]):
     user = join_attributes(user_register, current_user)
-    print(f'El current user a crear es {user}')
+    resp = requests.post(f"{USERS_URL}/users", json=user)
+    return JSONResponse(content=resp.json(), status_code=resp.status_code)
 
 
 @router.put('/')
 def update_user(user_update: RegisterUser,
                 current_user: Annotated[str, Depends(get_current_user)]):
     user = join_attributes(user_update, current_user)
-    print(f'El current user a actualizar es {user}')
+    resp = requests.put(f"{USERS_URL}/users", json=user)
+    return JSONResponse(content=resp.json(), status_code=resp.status_code)
 
 
 @router.delete('/')
 def delete_user(current_user: Annotated[str, Depends(get_current_user)]):
-    print(f'El current user a eliminar es {current_user["uid"]}')
+    resp = requests.delete(f"{USERS_URL}/users/{current_user['uid']}")
+    return JSONResponse(content=resp.json(), status_code=resp.status_code)
