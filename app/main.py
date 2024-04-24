2863fe8c8b048bf563f243dd3b6f78658d48ec45
mateocapon <mcapon@fi.uba.ar>
2024-04-24 14:52:34 -0300

    cors

--- app/main.py
+++ app/main.py
@@ -6,20 +6,12 @@ from .routers import users, swipes
 app = FastAPI()
 
 
-origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8080",
-]
-
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["Authorization", "Content-Type"],
 )
 
 app.include_router(users.router)
