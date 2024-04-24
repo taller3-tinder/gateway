8949e376e0e16bc57128ed5deb9abfbd952d76d6
mateocapon <mcapon@fi.uba.ar>
2024-04-24 17:15:26 -0300

    add swipes sec

--- app/routers/swipes.py
+++ app/routers/swipes.py
@@ -1,15 +1,18 @@
+from typing import Annotated
 from dotenv import load_dotenv
 import os
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from fastapi.responses import JSONResponse
 import requests
-from tinderlibs.schemas.swipes import SwipeBase
+from gateway.app.middleware.firebase import get_current_user
+from tinderlibs.schemas.swipes import UserSwipe
 from fastapi.encoders import jsonable_encoder
 
 
 router = APIRouter(
     prefix="/swipes",
-    tags=['Swipes']
+    tags=['Swipes'],
+    dependencies=[Depends(get_current_user)]
 )
 
 
@@ -18,10 +21,12 @@ SWIPES_URL = os.getenv("SWIPES_URL")
 
 
 @router.post('/')
-def add_user_swipe(swipe: SwipeBase):
+def add_user_swipe(swipe: UserSwipe,
+                   current_user: Annotated[dict, Depends(get_current_user)]):
     """
     Adds a Swipe to the Tinder System.
     """
     swipe = jsonable_encoder(swipe)
+    swipe['swiper'] = current_user["uid"]
     resp = requests.post(f"{SWIPES_URL}/swipes", json=swipe)
     return JSONResponse(content=resp.json(), status_code=resp.status_code)
